82f56be8919bcef63b04b90e190f49766a821be7
Farouk212022 <faroukabdala21@gmail.com>
2025-05-24 11:16:22 -0500

    Primer Commit

---
+++ src/main/java/com/pruebatecnica/carlosabdala/CarlosabdalaApplication.java
@@ -0,0 +1,13 @@
+package com.pruebatecnica.carlosabdala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarlosabdalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarlosabdalaApplication.class, args);
+	}
+
+}
